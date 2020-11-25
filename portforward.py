c4c573f89e4e38b207899f7c4b92f4fe61f5d555
friyadhibiermann <friyadhibiermann@gmail.com>
2020-11-26 03:37:40 +0700

    Update portforward.py

--- portforward.py
+++ portforward.py
@@ -1,24 +1,5 @@
 #!/usr/bin/env python
-#
-# ForwardProxy
-# Port Forwarding Proxy
-#
-# This is a port-forwarding proxy.
-# It connects to another server:port and returns the received data.
-# Useful for server-side redirects to local services or resources.
-# So you can hide services from public access, but enable controlled
-# access through the proxy.
-#
-# The embedded authentication module protects the proxy itself against
-# unauthorized access. This is only an example implementation, which works
-# with 2 plain text URL-Parameters. You can us it, how it is (you shouldn't!),
-# you can replace it with your own authentication module or you can disable
-# the authentication.
-#
-# Inspired by: https://gist.github.com/darkwave/52842722c0c451807df4
-#
-# Jann Westphal 02/2020
-#
+
 import sys
 import time
 import socket
